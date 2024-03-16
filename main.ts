262bd26c2332dd4496c212405eaa48819929cf5e
Muhammad Bilal Khan <160259391+Bilalkhan0026@users.noreply.github.com>
2024-03-15 23:42:41 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+const question = [
+    { type: 'numbar',name: 'weight',message: 'weight(kg):'},
+    {type: 'numbar',name: 'height',message: 'height(m):'},
+
+];
+const calculateBMI=(weight: number, height: number):number => weight/(height*height);
+const main = async() => {
+    const {weight,height}= await inquirer.prompt(question);
+    console.log(`BMI: ${calculateBMI(weight,height).toFixed(2)}`)
+};
+main();
